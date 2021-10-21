913ccbd6a0080db66e7332cbc76570c2dd774b7e
Yuki Kitayama <yk2797@columbia.edu>
2021-10-21 17:42:09 -0600

    still learning NextJS

--- nextjs-course/pages/news/index.js
+++ nextjs-course/pages/news/index.js
@@ -1,7 +1,20 @@
-// our-domain.com/news
+import { Fragment } from "react";
+import Link from 'next/link';
 
 function NewsPage() {
-  return <h1>The News Page</h1>
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS Is A Greate Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
