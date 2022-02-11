b4e0f7b152c7a1740cdce7efe16d44253aec5ccb
Yuki Kitayama <yk2797@columbia.edu>
2022-02-10 17:06:25 -0700

    reviewing useEffect

--- hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
+++ hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 
 import Card from "../UI/Card";
+import LoadingIndicator from '../UI/LoadingIndicator';
 import "./IngredientForm.css";
 
 const IngredientForm = React.memo((props) => {
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
 
   const submitHandler = (event) => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
   };
 
   return (
@@ -34,12 +35,13 @@ const IngredientForm = React.memo((props) => {
               id="amount"
               value={enteredAmount}
               onChange={(event) => {
-                setEnteredAmount(event.target.value)
+                setEnteredAmount(event.target.value);
               }}
             />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
+            {props.loading && <LoadingIndicator />}
           </div>
         </form>
       </Card>
